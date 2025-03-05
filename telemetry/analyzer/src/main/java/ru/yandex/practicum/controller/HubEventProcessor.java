20fe25c5ffbb39d776079df56a1916195ad71c1f
Alex H1dd3N <gumskull102012@yandex.ru>
2025-03-05 13:45:39 +0300

    Some fixes

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/controller/HubEventProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/controller/HubEventProcessor.java
@@ -8,8 +8,9 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.config.kafka.KafkaConfig;
-import ru.yandex.practicum.handler.SnapshotHandler;
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import ru.yandex.practicum.handler.HubHandler;
+import ru.yandex.practicum.handler.HubHandlerEvent;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 
 import java.time.Duration;
 import java.util.List;
@@ -17,23 +18,29 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class SnapshotController {
-    private final KafkaConsumer<String, SensorsSnapshotAvro> consumer;
+public class HubEventProcessor implements Runnable {
+    private final KafkaConsumer<String, HubEventAvro> consumer;
     private final KafkaConfig config;
-    private final SnapshotHandler handler;
+    private final HubHandler hubHandler;
 
-
-    public void start() {
+    @Override
+    public void run() {
         try {
-            consumer.subscribe(List.of(config.getSnapshotTopicIn()));
+            consumer.subscribe(List.of(config.getHubTopicIn()));
+            Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
             while (true) {
-                ConsumerRecords<String, SensorsSnapshotAvro> records = consumer.poll(Duration.ofMillis(1000));
+                ConsumerRecords<String, HubEventAvro> records =
+                        consumer.poll(Duration.ofMillis(1000));
                 if (records.isEmpty()) continue;
-                for (ConsumerRecord<String, SensorsSnapshotAvro> record : records) {
+                for (ConsumerRecord<String, HubEventAvro> record : records) {
                     log.info("Получено сообщение: {}, со смещением {}, из топика {}",
                             record.value(), record.offset(), record.topic());
-                    handler.handle(record.value());
-                    consumer.commitSync();
+                    HubHandlerEvent handler = hubHandler.getContext()
+                            .get(record.value().getPayload().getClass().getName());
+                    if (handler != null) {
+                        handler.handle(record.value());
+                        consumer.commitSync();
+                    }
                 }
             }
         } catch (WakeupException ignored) {
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/controller/SnapshotProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/controller/SnapshotProcessor.java
@@ -8,9 +8,8 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.config.kafka.KafkaConfig;
-import ru.yandex.practicum.handler.HubHandler;
-import ru.yandex.practicum.handler.HubHandlerEvent;
-import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.handler.SnapshotHandler;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 import java.time.Duration;
 import java.util.List;
@@ -18,29 +17,23 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class HubEventController implements Runnable {
-    private final KafkaConsumer<String, HubEventAvro> consumer;
+public class SnapshotProcessor {
+    private final KafkaConsumer<String, SensorsSnapshotAvro> consumer;
     private final KafkaConfig config;
-    private final HubHandler hubHandler;
+    private final SnapshotHandler handler;
 
-    @Override
-    public void run() {
+
+    public void start() {
         try {
-            consumer.subscribe(List.of(config.getHubTopicIn()));
-            Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
+            consumer.subscribe(List.of(config.getSnapshotTopicIn()));
             while (true) {
-                ConsumerRecords<String, HubEventAvro> records =
-                        consumer.poll(Duration.ofMillis(1000));
+                ConsumerRecords<String, SensorsSnapshotAvro> records = consumer.poll(Duration.ofMillis(1000));
                 if (records.isEmpty()) continue;
-                for (ConsumerRecord<String, HubEventAvro> record : records) {
+                for (ConsumerRecord<String, SensorsSnapshotAvro> record : records) {
                     log.info("Получено сообщение: {}, со смещением {}, из топика {}",
                             record.value(), record.offset(), record.topic());
-                    HubHandlerEvent handler = hubHandler.getContext()
-                            .get(record.value().getPayload().getClass().getName());
-                    if (handler != null) {
-                        handler.handle(record.value());
-                        consumer.commitSync();
-                    }
+                    handler.handle(record.value());
+                    consumer.commitSync();
                 }
             }
         } catch (WakeupException ignored) {
