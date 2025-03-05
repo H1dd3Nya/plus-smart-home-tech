10d31b532c143a4a757ec05161b03b50b4b26ef3
Alex H1dd3N <gumskull102012@yandex.ru>
2025-03-05 13:13:42 +0300

    Added gRPC

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/handler/sensor/SwitchSensorEventHandler.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.handler.sensor;
+
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import ru.yandex.practicum.grpc.telemetry.event.SwitchSensorProto;
+import ru.yandex.practicum.handler.SensorEventHandler;
+import ru.yandex.practicum.kafka.KafkaConfig;
+import ru.yandex.practicum.kafka.KafkaEventProducer;
+import ru.yandex.practicum.kafka.telemetry.event.SwitchSensorAvro;
+
+@Component
+public class SwitchSensorEventHandler extends SensorEventHandler<SwitchSensorAvro> {
+
+    public SwitchSensorEventHandler(KafkaConfig kafkaConfig, KafkaEventProducer kafkaEventProducer) {
+        super(kafkaConfig, kafkaEventProducer, SensorEventProto.PayloadCase.SWITCH_SENSOR);
+    }
+
+    @Override
+    protected SwitchSensorAvro mapToAvro(SensorEventProto event) {
+        if (event.hasSwitchSensor()) {
+            SwitchSensorProto switchSensor = event.getSwitchSensor();
+            return SwitchSensorAvro.newBuilder()
+                    .setState(switchSensor.getState())
+                    .build();
+        } else {
+            throw new IllegalArgumentException("Event does not contain switch sensor data");
+        }
+    }
+}
