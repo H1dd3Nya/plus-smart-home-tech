02c8756265189af7e01cd1abe5cd61b8fcc1bab3
Alex H1dd3N <gumskull102012@yandex.ru>
2025-03-17 17:55:04 +0300

    Fixes for analyzer

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/models/ActionType.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.models;
-
-public enum ActionType {
-    ACTIVATE,
-    DEACTIVATE,
-    SET_VALUE,
-    INVERSE
-}
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/repositories/ActionRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.models.Action;
-
-public interface ActionRepository extends JpaRepository<Action, Long> {
-}
