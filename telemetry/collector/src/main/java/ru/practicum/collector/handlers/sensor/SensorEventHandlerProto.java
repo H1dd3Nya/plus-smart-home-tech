2650b0269016a3020ad9feacca69c9c22d84061c
H1dd3Nya <gumskull102012@yandex.ru>
2025-04-09 20:50:52 +0300

    Added Discovery server and docker files

--- telemetry/collector/src/main/java/ru/practicum/collector/handlers/sensor/SensorEventHandlerProto.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.collector.handlers.sensor;
-
-import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
-
-public interface SensorEventHandlerProto {
-    SensorEventProto.PayloadCase getMessageType();
-
-    void handle(SensorEventProto event);
-}
